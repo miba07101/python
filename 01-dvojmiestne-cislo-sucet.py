bb611dabfd8bd95d2525d67d45f6f3062ae4f18e
vimi <vimi.vimi@dell>
2023-03-03 15:34:43 +0100

    name change

---
+++ 01-dvojmiestne-cislo-sucet.py
@@ -0,0 +1,7 @@
+u_number = input("Zadaj dvojmiestne cislo\n")
+
+# kazdu zlozku dvojmiestneho cisla [0]..[1] zmenim na integer a scitam
+sucet = int(u_number[0]) + int(u_number[1])
+
+# vypisem vysledky
+print(sucet)
