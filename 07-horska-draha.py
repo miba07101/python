24a98536290e93a425453871cce348211e6796ca
vimi <vimi.vimi@dell>
2023-03-07 11:36:09 +0100

    pr.7 horska-draha - david setek

---
+++ 07-horska-draha.py
@@ -0,0 +1,23 @@
+height = int(input("Aka je tvoja vyska? [cm]\n"))
+bill = 0
+
+if height > 100:
+    age = int(input("Aky je tvoj vek?\n"))
+    if age <= 12:
+        bill = 50
+        print(f"Cena listku je {bill} eur")
+    elif age <= 18:
+        bill = 100
+        print(f"Cena listku je {bill} eur")
+    else:
+        bill = 150
+        print(f"Cena listku je {bill} eur")
+    photo = input("Chces sa vyfotit? [ano-nie]\n")
+    if photo == "ano":
+        photo_price = 50
+        bill += photo_price
+        print(f"Cena za fotku je {photo_price} eur. Celkova cena je {bill} eur")
+    else:
+        print(f"Celkova cena je {bill} eur")
+else:
+    print("Zial, nemozes ist na horskej drahe")
