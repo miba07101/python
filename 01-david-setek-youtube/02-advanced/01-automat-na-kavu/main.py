eab82921a3bbc14e29365b30cb37095198d510f3
vimi <vimi.vimi@dell>
2023-03-20 11:41:46 +0100

    pr.1 automat-na-kavu

--- 01-david-setek-youtube/02-advanced/01-automat-na-kavu/main.py
+++ 01-david-setek-youtube/02-advanced/01-automat-na-kavu/main.py
@@ -33,23 +33,75 @@ def refund(price, user_sum_coins):
         )
 
 
+def ingrediences():
+    return resources
+
+
+def gen_allow_ing(name, ing):
+    ing["water"] = ing["water"] - MENU[name]["ingredients"]["water"]
+    ing["milk"] = ing["milk"] - MENU[name]["ingredients"]["milk"]
+    ing["coffee"] = ing["coffee"] - MENU[name]["ingredients"]["coffee"]
+    print(f"Zvysok surovin je: {ing}")
+    # return ing
+
+
+def allowable_ingrediences(choice):
+    if choice == "espresso":
+        gen_allow_ing(choice, allowable_ing)
+    if choice == "latte":
+        gen_allow_ing(choice, allowable_ing)
+    if choice == "cappuccino":
+        gen_allow_ing(choice, allowable_ing)
+
+
+def rest_ingrediences(rest_ing):
+    if rest_ing["water"] < 0:
+        print("Zial nemame dostatok surovin na pripravu vasho napoja")
+        return False
+    elif rest_ing["milk"] < 0:
+        print("Zial nemame dostatok surovin na pripravu vasho napoja")
+        return False
+    elif rest_ing["coffee"] < 0:
+        print("Zial nemame dostatok surovin na pripravu vasho napoja")
+        return False
+    else:
+        print("Mame dostatok surovin na pripravu vasho napoja")
+        return True
+
+
 # === hlavny program ===
-user_choice = input("Co by ste si dal? (esspresso, latte, cappuccino): ")
+# nacitam zostatok / mnozstvo dostupnych surovin
+allowable_ing = ingrediences()
 
-# mozem zadat a "report" pre vypis zostatku ingrediencii
-if user_choice == "report":
-    print(resources)
+lets_continue = True
+while lets_continue:
+    user_choice = input("Co by ste si dal? (espresso, latte, cappuccino): ")
 
-# volba napoja - hlavny program
-if user_choice == "espresso":
-    sum = coins()
-    print(f"Cena espressa je: {espresso_price} KC")
-    refund(espresso_price, sum)
-elif user_choice == "latte":
-    sum = coins()
-    print(f"Cena latte je: {espresso_price} KC")
-    refund(latte_price, sum)
-elif user_choice == "cappuccino":
-    sum = coins()
-    print(f"Cena cappuccina je: {espresso_price} KC")
-    refund(cappuccino_price, sum)
+    # vypocet zostatku surovin
+    allowable_ingrediences(user_choice)
+
+    # kontrola dostatku surovin
+    if user_choice != "report":
+        lets_continue = rest_ingrediences(allowable_ing)
+
+        # kontrola pokracovania cyklu while
+        if lets_continue is not True:
+            break
+
+    # mozem zadat aj "report" pre vypis zostatku surovin
+    if user_choice == "report":
+        print(allowable_ing)
+
+    # volba napoja - hlavny program
+    if user_choice == "espresso":
+        sum = coins()
+        print(f"Cena espressa je: {espresso_price} KC")
+        refund(espresso_price, sum)
+    elif user_choice == "latte":
+        sum = coins()
+        print(f"Cena latte je: {espresso_price} KC")
+        refund(latte_price, sum)
+    elif user_choice == "cappuccino":
+        sum = coins()
+        print(f"Cena cappuccina je: {espresso_price} KC")
+        refund(cappuccino_price, sum)
